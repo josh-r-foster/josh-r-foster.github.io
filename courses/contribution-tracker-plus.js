a41a634e155db109921308ed10a31b89ae107079
Joshua Foster <31811862+josh-r-foster@users.noreply.github.com>
2023-06-17 14:32:02 -0400

    contribution-tracker-plus-1

---
+++ courses/contribution-tracker-plus.js
@@ -0,0 +1,106 @@
+const container = document.getElementById('person-container');
+const exportButton = document.getElementById('export-button');
+const peopleData = [
+  { name: 'John Doe' },
+  { name: 'Jane Smith' },
+  { name: 'Michael Johnson' },
+  { name: 'Emily Davis' },
+  { name: 'Daniel Wilson' },
+  { name: 'Olivia Thompson' },
+];
+const buttonsData = ["Came prepared", "Strong problem solving", "Quality decision analysis", "Good listening", "Considered other perspectives", "Clear communication", "Distracted", "Arrived late", "Absent"];
+
+function createPersonBox(person) {
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  const heading = document.createElement('h2');
+  heading.textContent = person.name;
+  box.appendChild(heading);
+
+  const counter = document.createElement('div');
+  counter.className = 'counter';
+  box.appendChild(counter);
+
+  const decreaseButton = document.createElement('button');
+  decreaseButton.className = 'decrease';
+  decreaseButton.textContent = '-';
+  counter.appendChild(decreaseButton);
+
+  const countElement = document.createElement('span');
+  countElement.className = 'count';
+  countElement.textContent = '0';
+  counter.appendChild(countElement);
+
+  const increaseButton = document.createElement('button');
+  increaseButton.className = 'increase';
+  increaseButton.textContent = '+';
+  counter.appendChild(increaseButton);
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'button-container';
+  box.appendChild(buttonContainer);
+
+  buttonsData.forEach((buttonName) => {
+    const button = document.createElement('button');
+    button.textContent = buttonName;
+    button.className = 'button';
+
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+    });
+
+    buttonContainer.appendChild(button);
+  });
+
+  container.appendChild(box);
+
+  // Counter functionality
+  const decreaseButtonElement = box.querySelector('.decrease');
+  const increaseButtonElement = box.querySelector('.increase');
+
+  let count = 0;
+
+  decreaseButtonElement.addEventListener('click', () => {
+    count--;
+    countElement.textContent = count;
+  });
+
+  increaseButtonElement.addEventListener('click', () => {
+    count++;
+    countElement.textContent = count;
+  });
+}
+
+function exportData() {
+  const boxes = document.querySelectorAll('.box');
+  const data = [];
+
+  boxes.forEach((box) => {
+    const personData = {
+      person: box.querySelector('h2').textContent,
+      count: parseInt(box.querySelector('.count').textContent),
+      buttons: Array.from(box.querySelectorAll('.button')).map((button) => ({
+        name: button.textContent,
+        active: button.classList.contains('active'),
+      })),
+    };
+
+    data.push(personData);
+  });
+
+  const currentDate = new Date().toISOString().split('T')[0];
+  const fileName = `data_${currentDate}.json`;
+  const jsonData = JSON.stringify(data);
+
+  const link = document.createElement('a');
+  link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
+  link.download = fileName;
+  link.click();
+}
+
+peopleData.forEach((person) => {
+  createPersonBox(person);
+});
+
+exportButton.addEventListener('click', exportData);
