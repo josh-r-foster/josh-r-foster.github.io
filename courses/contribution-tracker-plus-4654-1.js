3b06d80a002d7ff7300688b140b5ae639adabf7f
Joshua Foster <31811862+josh-r-foster@users.noreply.github.com>
2023-09-09 11:39:09 -0400

    populated 4654 contribution tracker

---
+++ courses/contribution-tracker-plus-4654-1.js
@@ -0,0 +1,163 @@
+const container = document.getElementById('person-container');
+const exportButton = document.getElementById('export-button');
+
+const peopleData = [
+  {name: 'Aaron Myers', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Adrian Benedikt Wolf', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Alain Philippe Konan', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Alan Guan', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Alex Donnelly', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Alex Larwill', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Amanda Bano', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Amanuel Gebeyehu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Amir Shams-Ansari', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Anjana Somasundaram', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Annie Chen', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Beau Lapointe', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Bona Kim', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Brendan Chambers', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Carol Guo', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Caroline An', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Charles Johnson', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Chris Gratsas', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Cindy Zhang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Cody Hudson', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Corey Gilmore', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Ellen Kempton', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Elsie Gao', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jennica Cai', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jennifer Zhang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jonathan Hader', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Karina Zhang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Lauris Rosenbusch', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Luke Sinclair', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Maciej Monko', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Maya Wolfman', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Ningsong Shen', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Nizar Hammour', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Noah Lubov', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Peter Hansen', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rajan Deshpande', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Ramtin Abdi', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Resham Arora', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Robert Flaim', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rosy Ren', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rowena Shi', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Sam Coleman-Shapiro', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Tamara Trklja', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Vanessa Fuda', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Wendy Chi', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Winnie Huang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Yekai Fan', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Zane Chiang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+];
+const buttonsData = ["Came prepared", "Strong problem solving", "Quality decision analysis", "Good listening", "Considered other perspectives", "Insightful connections", "Clear communication", "Distracted", "Arrived late", "Absent"];
+
+function createPersonBox(person) {
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  const boxHeader = document.createElement('div');
+  boxHeader.className = 'box-header';
+  box.appendChild(boxHeader);
+
+  const profilePicture = document.createElement('img');
+  profilePicture.className = 'profile-picture';
+  profilePicture.src = person.profilePictureUrl; // Set the source of profile picture from person data
+  profilePicture.alt = person.name + "'s profile picture";
+  boxHeader.appendChild(profilePicture);
+
+  const nameAndCounter = document.createElement('div');
+  nameAndCounter.className = 'name-and-counter';
+  boxHeader.appendChild(nameAndCounter);
+
+  const heading = document.createElement('h2');
+  heading.textContent = person.name;
+  nameAndCounter.appendChild(heading);
+
+  const counter = document.createElement('div');
+  counter.className = 'counter';
+  nameAndCounter.appendChild(counter);
+
+  const decreaseButton = document.createElement('button');
+  decreaseButton.className = 'decrease';
+  decreaseButton.textContent = '-';
+  counter.appendChild(decreaseButton);
+
+  const countElement = document.createElement('span');
+  countElement.className = 'count';
+  countElement.textContent = '0';
+  counter.appendChild(countElement);
+
+  const increaseButton = document.createElement('button');
+  increaseButton.className = 'increase';
+  increaseButton.textContent = '+';
+  counter.appendChild(increaseButton);
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'button-container';
+  box.appendChild(buttonContainer);
+
+  buttonsData.forEach((buttonName) => {
+    const button = document.createElement('button');
+    button.textContent = buttonName;
+    button.className = 'button';
+
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+    });
+
+    buttonContainer.appendChild(button);
+  });
+
+  container.appendChild(box);
+
+  // Counter functionality
+  const decreaseButtonElement = box.querySelector('.decrease');
+  const increaseButtonElement = box.querySelector('.increase');
+
+  let count = 0;
+
+  decreaseButtonElement.addEventListener('click', () => {
+    count--;
+    countElement.textContent = count;
+  });
+
+  increaseButtonElement.addEventListener('click', () => {
+    count++;
+    countElement.textContent = count;
+  });
+}
+
+function exportData() {
+  const boxes = document.querySelectorAll('.box');
+  const data = [];
+
+  boxes.forEach((box) => {
+    const personData = {
+      person: box.querySelector('h2').textContent,
+      count: parseInt(box.querySelector('.count').textContent),
+      buttons: Array.from(box.querySelectorAll('.button')).map((button) => ({
+        name: button.textContent,
+        active: button.classList.contains('active'),
+      })),
+    };
+
+    data.push(personData);
+  });
+
+  const currentDate = new Date().toISOString().split('T')[0];
+  const fileName = `contribution_data_${currentDate}.json`;
+  const jsonData = JSON.stringify(data);
+
+  const link = document.createElement('a');
+  link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
+  link.download = fileName;
+  link.click();
+}
+
+peopleData.forEach((person) => {
+  createPersonBox(person);
+});
+
+exportButton.addEventListener('click', exportData);
---
+++ courses/contribution-tracker-plus-4654-2.js
@@ -0,0 +1,163 @@
+const container = document.getElementById('person-container');
+const exportButton = document.getElementById('export-button');
+
+const peopleData = [
+  {name: 'Alexander Flannery', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Alexandra Nicolescu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Amberly Zhou', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Andrew Lukezic', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Annie Lai', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Anthony De Rango', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Arlo Coleman-Shapiro', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Arman Valyani-Perera', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Colin Chataway', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Eric Li', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Eric Wang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Grace Okamura', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Iliyan Bhanji', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jackson MacPhie', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jacky Huang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jasmine Chen', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jasmine Kordbacheh', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jessica Bian', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jessie Wu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'John Liu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jomana Elsays', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Judy Zhang', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Jukka Schotter', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Julia Ionescu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Katie Thien', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Kenan Xu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Laura Tolciu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Lisa Jia', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Matthew Sandford', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Megan Buitendag', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Michael Chi', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Michael Hawkes', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Nick Bennett', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Noah Behar', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Paige Murray', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rachel Bai', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rachel Lee', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Rafa Drumond', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Renee Xie', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Ronak Lad', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Sophia Lu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Srushti Patel', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Tiffany Too', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Valerie Ah-Yen', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Valerie Phu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'William Chan', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Yong Yu', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+  {name: 'Zachary Clarke', profilePictureUrl: '/images/teaching/default-profile-picture.jpg'},
+];
+const buttonsData = ["Came prepared", "Strong problem solving", "Quality decision analysis", "Good listening", "Considered other perspectives", "Insightful connections", "Clear communication", "Distracted", "Arrived late", "Absent"];
+
+function createPersonBox(person) {
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  const boxHeader = document.createElement('div');
+  boxHeader.className = 'box-header';
+  box.appendChild(boxHeader);
+
+  const profilePicture = document.createElement('img');
+  profilePicture.className = 'profile-picture';
+  profilePicture.src = person.profilePictureUrl; // Set the source of profile picture from person data
+  profilePicture.alt = person.name + "'s profile picture";
+  boxHeader.appendChild(profilePicture);
+
+  const nameAndCounter = document.createElement('div');
+  nameAndCounter.className = 'name-and-counter';
+  boxHeader.appendChild(nameAndCounter);
+
+  const heading = document.createElement('h2');
+  heading.textContent = person.name;
+  nameAndCounter.appendChild(heading);
+
+  const counter = document.createElement('div');
+  counter.className = 'counter';
+  nameAndCounter.appendChild(counter);
+
+  const decreaseButton = document.createElement('button');
+  decreaseButton.className = 'decrease';
+  decreaseButton.textContent = '-';
+  counter.appendChild(decreaseButton);
+
+  const countElement = document.createElement('span');
+  countElement.className = 'count';
+  countElement.textContent = '0';
+  counter.appendChild(countElement);
+
+  const increaseButton = document.createElement('button');
+  increaseButton.className = 'increase';
+  increaseButton.textContent = '+';
+  counter.appendChild(increaseButton);
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'button-container';
+  box.appendChild(buttonContainer);
+
+  buttonsData.forEach((buttonName) => {
+    const button = document.createElement('button');
+    button.textContent = buttonName;
+    button.className = 'button';
+
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+    });
+
+    buttonContainer.appendChild(button);
+  });
+
+  container.appendChild(box);
+
+  // Counter functionality
+  const decreaseButtonElement = box.querySelector('.decrease');
+  const increaseButtonElement = box.querySelector('.increase');
+
+  let count = 0;
+
+  decreaseButtonElement.addEventListener('click', () => {
+    count--;
+    countElement.textContent = count;
+  });
+
+  increaseButtonElement.addEventListener('click', () => {
+    count++;
+    countElement.textContent = count;
+  });
+}
+
+function exportData() {
+  const boxes = document.querySelectorAll('.box');
+  const data = [];
+
+  boxes.forEach((box) => {
+    const personData = {
+      person: box.querySelector('h2').textContent,
+      count: parseInt(box.querySelector('.count').textContent),
+      buttons: Array.from(box.querySelectorAll('.button')).map((button) => ({
+        name: button.textContent,
+        active: button.classList.contains('active'),
+      })),
+    };
+
+    data.push(personData);
+  });
+
+  const currentDate = new Date().toISOString().split('T')[0];
+  const fileName = `contribution_data_${currentDate}.json`;
+  const jsonData = JSON.stringify(data);
+
+  const link = document.createElement('a');
+  link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
+  link.download = fileName;
+  link.click();
+}
+
+peopleData.forEach((person) => {
+  createPersonBox(person);
+});
+
+exportButton.addEventListener('click', exportData);
